c6cf78abd7dcac04ca4f7a94f4d567ba5fa5de15
Taraikovich <71258577+Taraikovich@users.noreply.github.com>
2024-07-15 18:16:53 +0400

    feat: upload project

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, inject } from '@angular/core';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name?: string;
+  isAddingTask = false;
+  private tasksService = inject(TasksService);
+
+  get selectedUsersTasks() {
+    return this.tasksService.getUserTasks(this.userId)
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
